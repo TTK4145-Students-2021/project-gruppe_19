14e32851354c43d9e0f16a501ad37a6ffc8cfc91
JacobHDahl <jacob.h.dahl@hotmail.no>
2021-04-18 11:04:59 +0200

    fixed run-script

--- run_elevs.sh
+++ run_elevs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "Velkommen til heis!"
 echo "Alt er hardkoda nå:)"
-cd ..
 cd Simulator-v2
 gnome-terminal -x ./SimElevatorServer --port=15555
 sleep 1
@@ -9,7 +8,6 @@ gnome-terminal -x ./SimElevatorServer --port=15444
 sleep 1
 gnome-terminal -x ./SimElevatorServer --port=15333
 cd ..
-cd project-gruppe_19
 gnome-terminal -x go run main.go -elev_port=15555 -transmit_port=1111 -receive_port=2222 -receive_port2=3333 -elev_id="forste"
 sleep 1
 gnome-terminal -x go run main.go -elev_port=15444 -transmit_port=2222 -receive_port=1111 -receive_port2=3333 -elev_id="andre"
