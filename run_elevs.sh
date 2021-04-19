a24fa189d0277bc62e98a07a7b0ce899b166c3ad
JacobHDahl <jacob.h.dahl@hotmail.no>
2021-04-19 10:15:29 +0200

    changed ID's to a number so it's usable in the code. im stupid

--- run_elevs.sh
+++ run_elevs.sh
@@ -8,11 +8,11 @@ gnome-terminal -x ./SimElevatorServer --port=15444
 sleep 1
 gnome-terminal -x ./SimElevatorServer --port=15333
 cd ..
-gnome-terminal -x go run main.go -elev_port=15555 -transmit_port=1111 -receive_port=2222 -receive_port2=3333 -elev_id="forste"
+gnome-terminal -x go run main.go -elev_port=15555 -transmit_port=1111 -receive_port=2222 -receive_port2=3333 -elev_id="1"
 sleep 1
-gnome-terminal -x go run main.go -elev_port=15444 -transmit_port=2222 -receive_port=1111 -receive_port2=3333 -elev_id="andre"
+gnome-terminal -x go run main.go -elev_port=15444 -transmit_port=2222 -receive_port=1111 -receive_port2=3333 -elev_id="2"
 sleep 1
-go run main.go -elev_port=15333 -transmit_port=3333 -receive_port=1111 -receive_port2=2222 -elev_id="tredje"
+go run main.go -elev_port=15333 -transmit_port=3333 -receive_port=1111 -receive_port2=2222 -elev_id="3"
 
 
 
